7e53f9ec4c2d8887f45a2a872a4097132452561b
sujir <sujir@Preetha_98>
2025-05-08 21:01:53 +0530

    Commit

--- src/Examples/Calculator.java
+++ src/Examples/Calculator.java
@@ -6,7 +6,7 @@ public class Calculator {
         return a + b;
     }
 
-    public int subtract(int a, int b) {
+    public int sub(int a, int b) {
         return a - b;
     }
 
@@ -25,7 +25,7 @@ public class Calculator {
 		Calculator calc = new Calculator();
 
         System.out.println("Addition: " + calc.add(10, 5));
-        System.out.println("Subtraction: " + calc.subtract(10, 5));
+        System.out.println("Subtraction: " + calc.sub(10, 5));
         System.out.println("Multiplication: " + calc.multiply(10, 5));
         System.out.println("Division: " + calc.divide(10, 5));
     }
